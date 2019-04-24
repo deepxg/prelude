63fa658bb7931ec668ea4b3180588d4381d23b22
Thom Lawrence <thom@deepxg.com>
2019-04-24 15:07:04 +0100

    Add clj-refactor.

--- personal/clojure.el
+++ personal/clojure.el
@@ -5,13 +5,14 @@
 
 (add-hook 'clojure-mode-hook 'aggressive-indent-mode)
 (add-hook 'clojure-mode-hook 'paredit-mode)
+(add-hook 'clojure-mode-hook 'clj-refactor-mode)
 (add-hook 'cider-mode-hook
           (lambda ()
             (add-hook 'after-save-hook
                       (lambda ()
                         (when (clojure-find-ns)
                           (cider-load-buffer)
-                          (cider-test-run-loaded-tests)))
+                          (cider-test-run-loaded-tests nil)))
                       nil t)))
 
 (when (string-equal system-type "windows-nt")
