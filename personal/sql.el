800478e1df1bd7d3d3f0cd363164af4c434a1330
Thom Lawrence <thom@deepxg.com>
2019-04-24 15:07:18 +0100

    Improve initial SQL environment.

--- personal/sql.el
+++ personal/sql.el
@@ -1,1 +1,3 @@
 (add-hook 'sql-interactive-mode-hook 'sql-set-sqli-buffer-generally)
+(add-hook 'sql-interactive-mode-hook 'toggle-truncate-lines)
+(add-hook 'sql-interactive-mode-hook (lambda () (set-buffer-process-coding-system "utf-8" "utf-8")))
