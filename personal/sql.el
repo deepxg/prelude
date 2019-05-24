842d88b1c181a763c9430e972eaf792144ffc639
Thom Lawrence <thom@deepxg.com>
2019-05-24 10:31:47 +0100

    Allow port to be specified for psql.

--- personal/sql.el
+++ personal/sql.el
@@ -1,3 +1,8 @@
 (add-hook 'sql-interactive-mode-hook 'sql-set-sqli-buffer-generally)
 (add-hook 'sql-interactive-mode-hook 'toggle-truncate-lines)
-(add-hook 'sql-interactive-mode-hook (lambda () (set-buffer-process-coding-system "utf-8" "utf-8")))
+(add-hook 'sql-interactive-mode-hook (lambda () (set-buffer-process-coding-system 'utf-8 'utf-8)))
+(setq sql-postgres-login-params (quote ((user :default "Thom")
+                                        (database :default "Thom")
+                                        server
+                                        (port :default 5432))))
+(add-to-list 'sql-postgres-options "--set=sslmode=require" t)
