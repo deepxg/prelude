e46fd0bbf2da93f01a63045adbec9acc39690fd4
Thom Lawrence <thom@deepxg.com>
2019-06-01 22:46:13 +0100

    Add require.

--- personal/sql.el
+++ personal/sql.el
@@ -1,3 +1,4 @@
+(require 'sql)
 (add-hook 'sql-interactive-mode-hook 'sql-set-sqli-buffer-generally)
 (add-hook 'sql-interactive-mode-hook 'toggle-truncate-lines)
 (add-hook 'sql-interactive-mode-hook (lambda () (set-buffer-process-coding-system 'utf-8 'utf-8)))
