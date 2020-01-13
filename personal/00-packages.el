fa568997802085a51dd3d87f4843e1b7ea73e94c
Thom Lawrence <thom@statsbomb.com>
2020-01-13 10:52:19 +0000

    Some new Python packages.

--- personal/00-packages.el
+++ personal/00-packages.el
@@ -1,1 +1,1 @@
-(prelude-require-packages '(paredit aggressive-indent cider exec-path-from-shell company))
+(prelude-require-packages '(paredit aggressive-indent cider exec-path-from-shell company elpy ein py-autopep8 blacken))
