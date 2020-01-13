fd6b1058203d7f601048a4d5e3d3767cfdc62251
Thom Lawrence <thom@statsbomb.com>
2020-01-13 10:52:12 +0000

    Fullscreen key binding.

--- personal/window.el
+++ personal/window.el
@@ -4,4 +4,5 @@
 (global-set-key (kbd "C-S-f") 'windmove-right)
 (global-set-key (kbd "C-S-p") 'windmove-up)
 (global-set-key (kbd "C-S-n") 'windmove-down)
+(global-set-key (kbd "M-RET") 'toggle-frame-fullscreen)
 (setq windmove-wrap-around t)
